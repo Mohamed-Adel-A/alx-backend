057277ad9fce578f6b16f1ee6eb26391f1927cdc
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-10 17:56:56 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,7 +31,7 @@ class Server:
         """Return the appropriate page of the dataset."""
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
+        start, end = self.index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
             return []
