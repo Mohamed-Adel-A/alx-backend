7d8465c1334aaa558a01ff69db16588f0ede265c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-10 20:29:11 +0200

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -3,13 +3,44 @@
 Hypermedia pagination
 """
 
-from typing import List, Dict
-from 0-simple_pagination import Server
+import csv
+import math
+from typing import List, Tuple, Dict
 
 
-class Server(Server):
-    """Enhanced Server class with hypermedia pagination."""
-    
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of start and end indexes."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset."""
+        assert(isinstance(page, int) and isinstance(page_size, int))
+        assert(page > 0 and page_size > 0)
+        [start, end] = index_range(page, page_size)
+        return self.dataset()[start: end]
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Return hypermedia pagination information."""
         data = self.get_page(page, page_size)
