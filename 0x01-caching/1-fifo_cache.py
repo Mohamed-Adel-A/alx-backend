27e7fd64c12679ea016e5bd3d12d9e4bfd72b67a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-10 21:54:18 +0200

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Defines FIFOCache class"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """Defines a caching system with FIFO eviction policy"""
+
+    def __init__(self):
+        """Initializes the FIFOCache instance"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Adds an item to the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded = self.queue.pop(0)
+            del self.cache_data[discarded]
+            print("DISCARD:", discarded)
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """Retrieves an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
