c3faefcee981ea0c1ee3ae4d5189f669b5821593
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-11 01:49:38 +0200

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Index route"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
