732d957445d39a6e40447e79201f00b0da87800e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-10 21:54:01 +0200

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Defines BasicCache class"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Defines a basic caching system"""
+
+    def __init__(self):
+        """Initializes the BasicCache instance"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds an item to the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
