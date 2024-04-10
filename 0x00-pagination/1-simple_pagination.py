1a84c99f7846a37747689a044676353fb9f74bd4
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-10 17:54:20 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,8 +32,10 @@ class Server:
     
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset."""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        start_index, end_index = self.index_range(page, page_size)
-        return self.dataset()[start_index:end_index]
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
