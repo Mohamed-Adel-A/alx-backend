bb06bba960a2cf8333a12dee06d76abc379efcb6
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-11 02:09:34 +0200

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Force locale with URL parameter
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
