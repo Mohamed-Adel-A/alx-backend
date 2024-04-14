b3817b8150fce2d8faef69d827feb34c922ae96f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-15 01:08:58 +0200

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-"""
-Get locale from request
-"""
+"""Parametrize templates"""
+
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
+import os
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
-    """Config class"""
+    """Config class for setting up languages"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """Get locale from request"""
-    return request.accept_languages.best_match(
-        app.config['LANGUAGES'])
+    """Determine the best match for the supported languages based on the request's accepted languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    """Index route"""
-    return render_template('2-index.html')
+    """Render index.html with parametrized templates"""
+    title = _("home_title")
+    header = _("home_header")
+    return render_template('3-index.html', title=title, header=header)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
